8ec65bebadcb07187a5cda9cb5a4ef8d53ef315c
Dominic Lopez <drlopez@berkeley.edu>
2026-08-08 11:51:14 +0000

    Update CHIPS 3.7 Wordguesser app

--- 3.7 CHIPS Wordguesser/app.rb
+++ 3.7 CHIPS Wordguesser/app.rb
@@ -1,7 +1,11 @@
-require 'sinatra'
+require 'sinatra/base'
 
 class MyApp < Sinatra::Base
+  set :host_authorization, {
+    permitted_hosts: ['.codio.io', 'localhost', '127.0.0.1']
+  }
+
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+    "<!DOCTYPE html><html><head></head><body><h1>Your Mother</h1></body></html>"
   end
 end
